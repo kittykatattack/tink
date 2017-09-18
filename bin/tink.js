7dfa4530f753d8942cb80bed11567df71fd5fb57
kittykatattack <dandylion13@gmail.com>
2017-09-18 14:46:23 -0400

    disabled pevent default on the upHandler to prevent input range slider elements from getting trapped in Firefox

--- bin/tink.js
+++ bin/tink.js
@@ -301,7 +301,10 @@ var Tink = (function () {
 
           //Call the `release` method if it's been assigned
           if (this.release) this.release();
-          event.preventDefault();
+
+          //`event.preventDefault();` needs to be disabled to prevent <input> range sliders
+          //from getting trapped in Firefox (and possibly Safari)
+          //event.preventDefault();
         },
 
         //The pointer's `touchendHandler`
@@ -322,7 +325,8 @@ var Tink = (function () {
 
           //Call the `release` method if it's been assigned
           if (this.release) this.release();
-          event.preventDefault();
+
+          //event.preventDefault();
         },
 
         //`hitTestSprite` figures out if the pointer is touching a sprite
--- src/tink.js
+++ src/tink.js
@@ -284,7 +284,10 @@ class Tink {
 
         //Call the `release` method if it's been assigned
         if (this.release) this.release();
-        event.preventDefault();
+
+        //`event.preventDefault();` needs to be disabled to prevent <input> range sliders
+        //from getting trapped in Firefox (and possibly Safari)
+        //event.preventDefault();
       },
 
       //The pointer's `touchendHandler`
@@ -305,7 +308,8 @@ class Tink {
 
         //Call the `release` method if it's been assigned
         if (this.release) this.release();
-        event.preventDefault();
+        
+        //event.preventDefault();
       },
 
       //`hitTestSprite` figures out if the pointer is touching a sprite
